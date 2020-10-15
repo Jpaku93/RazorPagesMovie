2801abd6ca24e0d329e75ffa7b933b1510eaf095
Johnson Paku <j.r.paku@hotmail.co.nz>
2020-10-16 12:05:24 +1300

    Movie Index

---
+++ RazorPagesMovie/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "D.O.B")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        /* could lead to maybe recomendations
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+        */
+        /* could lead to age rating restriction ?
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; } 
+        */
+    }
+}
